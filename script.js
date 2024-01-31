e3e26f5b57c1dcc8cb1468d3f9005137accd9de5
Bakhat Begum <bakhtbagum@gmail.com>
2024-01-31 13:08:45 +0000

    implementing function on how to get allEpisodes

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 //You can edit ALL of the code here
-function setup() {
+function setup(num) {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerText = "";
+  episodeList.forEach((episode) =>{
+    const episodeDiv = document.createElement("div");
+    episodeDiv.classList.add("div");
+    episodeDiv.innerHTML = `
+        <h2 id ="forEpisode">${episode.name}</h2>
+        <p id ="forEpisode"> - S${episodeSeason(episode.season)}E${episodeSeason(episode.number)}</p>
+        <img src = "${episode.image.medium}" alt="${episode.name}">
+        <p>${episode.summary}</p>
+    ` 
+    rootElem.appendChild(episodeDiv);
+  });
+}
+
+function episodeSeason(num){
+  let numString = num.toString();
+  let formattedNum = numString.padStart(2, '0');
+  return formattedNum;
 }
 
 window.onload = setup;

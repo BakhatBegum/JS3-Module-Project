291cadc0fea6f6c1ce57f64d402735c55e34d82c
Sabella-8 <gsabella549@gmail.com>
2024-02-06 12:07:15 +0000

    Adding a search input that filters films

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //You can edit ALL of the code here
 function setup(num) {
-  const allEpisodes = getAllEpisodes();
+  const allEpisodes = state.getAllEpisodes;
   makePageForEpisodes(allEpisodes);
 }
 
@@ -8,23 +8,38 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   rootElem.innerText = "";
-  episodeList.forEach((episode) =>{
+  episodeList.forEach((episode) => {
     const episodeDiv = document.createElement("div");
     episodeDiv.classList.add("div");
     episodeDiv.innerHTML = `
-        <h2 id ="forEpisode">${episode.name} - S${episodeSeason(episode.season)}E${episodeSeason(episode.number)}</h2>
+        <h2 id ="forEpisode">${episode.name} - S${episodeSeason(
+      episode.season
+    )}E${episodeSeason(episode.number)}</h2>
        
  <img src = "${episode.image.medium}" alt="${episode.name}">
         <p>${episode.summary}</p>
-    ` 
+    `;
     rootElem.appendChild(episodeDiv);
   });
+
+  function episodeSeason(num) {
+    let numString = num.toString();
+    let formattedNum = numString.padStart(2, "0");
+    return formattedNum;
+  }
 }
 
-function episodeSeason(num){
-  let numString = num.toString();
-  let formattedNum = numString.padStart(2, '0');
-  return formattedNum;
+function render() {
+  const searching = document.getElementById("search-bar");
+  const allEpisodes = state.getAllEpisodes;
+  searching.addEventListener("input", function () {
+    state.searchTerm = searching.value.toLowerCase();
+    const filteredEpisodes = allEpisodes.filter((episode) => {
+      return episode.name.toLowerCase().includes(state.searchTerm);
+    });
+    makePageForEpisodes(filteredEpisodes);
+  });
 }
+render();
 
-window.onload = setup;
+window.onload = setup();

dd060043f8eac0fdcfa42bab8b22d96c8069e51e
Bakhat Begum <bakhtbagum@gmail.com>
2024-02-12 15:39:25 +0000

    Using Fetch to get all the episodes name, number, season,  and images

--- script.js
+++ script.js
@@ -1,50 +1,75 @@
-//You can edit ALL of the code here
-function setup(num) {
-  const allEpisodes = state.getAllEpisodes;
-  makePageForEpisodes(allEpisodes);
-  const numb = document.getElementById("num-epis");
-  numb.textContent = " " + allEpisodes.length + " ";
+// //You can edit ALL of the code here
+// function setup(num) {
+//   const allEpisodes = state.getAllEpisodes;
+//   makePageForEpisodes(allEpisodes);
+//   const numb = document.getElementById("num-epis");
+//   numb.textContent = " " + allEpisodes.length + " ";
+// }
+
+
+const rootElem = document.getElementById("root");
+
+function showLoading(){
+   rootElem.innerText = '<p>Loading...</p>'
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function showError(){
+  rootElem.innerText = '<p>Error loading data. Please try again later.</p>'
+}
+
+function fetchFilms(){
+  return fetch('https://api.tvmaze.com/shows/82/episodes')
+  .then(function (data) {
+    if (!data.ok){
+      throw new Error("Failed to fetch the data");
+    }
+     return data.json()
+  })
+}
+
+showLoading()
+
+fetchFilms().then(function (episodeList) {
+  //const rootElem = document.getElementById("root");
   rootElem.innerText = "";
+
   episodeList.forEach((episode) => {
+    const number = episode.number.toString().padStart(2, '0');
+    const season = episode.season.toString().padStart(2, '0');
     const episodeDiv = document.createElement("div");
     episodeDiv.classList.add("div");
     episodeDiv.innerHTML = `
-        <h2 id ="forEpisode">${episode.name} - S${episodeSeason(
-      episode.season
-    )}E${episodeSeason(episode.number)}</h2>
-       
- <img src = "${episode.image.medium}" alt="${episode.name}">
+        <h2 id="forEpisode">${episode.name} - S${season}E${number}</h2>
+         <img src = "${episode.image.medium}" alt="${episode.name}">
         <p>${episode.summary}</p>
     `;
     rootElem.appendChild(episodeDiv);
   });
-  function episodeSeason(num) {
-    let numString = num.toString();
-    let formattedNum = numString.padStart(2, "0");
-    return formattedNum;
-  }
-}
-function render() {
-  const searching = document.getElementById("search-bar");
-  const allEpisodes = state.getAllEpisodes;
-  searching.addEventListener("input", function () {
-    state.searchTerm = searching.value.toLowerCase();
-    const filteredEpisodes = allEpisodes.filter((episode) => {
-      return (
-        episode.name.toLowerCase().includes(state.searchTerm) ||
-        episode.summary.toLowerCase().includes(state.searchTerm)
-      );
-    });
-    const numb = document.getElementById("num-epis");
-    numb.textContent =
-      " " + filteredEpisodes.length + "/" + allEpisodes.length + " ";
-    makePageForEpisodes(filteredEpisodes);
-  });
-}
-render();
-window.onload = setup;
+}).catch(function (){
+  showError;
+});
+
+
+// function render() {
+//   const searching = document.getElementById("search-bar");
+//   const allEpisodes = state.getAllEpisodes;
+//   searching.addEventListener("input", function () {
+//     state.searchTerm = searching.value.toLowerCase();
+//     const filteredEpisodes = allEpisodes.filter((episode) => {
+//       return (
+//         episode.name.toLowerCase().includes(state.searchTerm) ||
+//         episode.summary.toLowerCase().includes(state.searchTerm)
+//       );
+//     });
+//     const numb = document.getElementById("num-epis");
+//     numb.textContent =
+//       " " + filteredEpisodes.length + "/" + allEpisodes.length + " ";
+//     makePageForEpisodes(filteredEpisodes);
+//   });
+// }
+// render();
+// window.onload = setup;
+
+
+
+

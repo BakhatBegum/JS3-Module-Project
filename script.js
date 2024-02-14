2e30a376b27c211e49179d21fbaadbb610e95253
Sabella-8 <gsabella549@gmail.com>
2024-02-14 16:10:51 +0000

    adding select element

--- script.js
+++ script.js
@@ -6,49 +6,50 @@
 //   numb.textContent = " " + allEpisodes.length + " ";
 // }
 
-
 const rootElem = document.getElementById("root");
 
-function showLoading(){
-   rootElem.innerText = '<p>Loading...</p>'
+function showLoading() {
+  rootElem.innerText = "<p>Loading...</p>";
 }
 
-function showError(){
-  rootElem.innerText = '<p>Error loading data. Please try again later.</p>'
+function showError() {
+  rootElem.innerText = "<p>Error loading data. Please try again later.</p>";
 }
 
-function fetchFilms(){
-  return fetch('https://api.tvmaze.com/shows/82/episodes')
-  .then(function (data) {
-    if (!data.ok){
-      throw new Error("Failed to fetch the data");
+function fetchFilms() {
+  return fetch("https://api.tvmaze.com/shows/36947/resident-alien").then(
+    function (data) {
+      if (!data.ok) {
+        throw new Error("Failed to fetch the data");
+      }
+      return data.json();
     }
-     return data.json()
-  })
+  );
 }
 
-showLoading()
+showLoading();
 
-fetchFilms().then(function (episodeList) {
-  //const rootElem = document.getElementById("root");
-  rootElem.innerText = "";
+fetchFilms()
+  .then(function (episodeList) {
+    //const rootElem = document.getElementById("root");
+    rootElem.innerText = "";
 
-  episodeList.forEach((episode) => {
-    const number = episode.number.toString().padStart(2, '0');
-    const season = episode.season.toString().padStart(2, '0');
-    const episodeDiv = document.createElement("div");
-    episodeDiv.classList.add("div");
-    episodeDiv.innerHTML = `
+    episodeList.forEach((episode) => {
+      const number = episode.number.toString().padStart(2, "0");
+      const season = episode.season.toString().padStart(2, "0");
+      const episodeDiv = document.createElement("div");
+      episodeDiv.classList.add("div");
+      episodeDiv.innerHTML = `
         <h2 id="forEpisode">${episode.name} - S${season}E${number}</h2>
          <img src = "${episode.image.medium}" alt="${episode.name}">
         <p>${episode.summary}</p>
     `;
-    rootElem.appendChild(episodeDiv);
+      rootElem.appendChild(episodeDiv);
+    });
+  })
+  .catch(function () {
+    showError;
   });
-}).catch(function (){
-  showError;
-});
-
 
 // function render() {
 //   const searching = document.getElementById("search-bar");
@@ -69,7 +70,3 @@ fetchFilms().then(function (episodeList) {
 // }
 // render();
 // window.onload = setup;
-
-
-
-
